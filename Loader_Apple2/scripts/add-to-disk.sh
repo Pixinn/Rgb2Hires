b20522f5a1feb35e9d2747e53ee0fa7e16bb1115
Christophe Meneboeuf <christophe@xtof.info>
2021-01-25 23:00:46 +0100

    Add helper scripts

---
+++ Loader_Apple2/scripts/add-to-disk.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Adds the required files to the provided disk.dsk
+# usage: add_to_disk PATH_TO_APPLECOMMANDER.jar PATH_TO_BINARY.a2 PATH_TO_PICTURE PATH_TO_DISK
+
+set -e
+
+if (( $# != 4 )); then
+    echo "Bad number of arguments"
+    echo "usage: add_to_disk.sh PATH_TO_APPLECOMMANDER.jar PATH_TO_BINARY.a2 PATH_TO_PICTURE PATH_TO_DISK"
+    exit
+fi
+
+echo " . revoving previous instance of DISPLAY form the disk"
+java -jar ${1} -d ${4} DISPLAY
+
+echo " .. adding DISPLAY to the disk"
+java -jar ${1} -cc65 ${4} DISPLAY BIN < ${2}
+
+echo " . revoving previous instance of PICTURE form the disk"
+java -jar ${1} -d ${4} PICTURE
+
+# echo " .. adding PICTURE to the disk"
+java -jar ${1} -cc65 ${4} PICTURE BIN < ${3}
+
+echo "DONE."
--- Rgb2Hires_PC/src/Common.h
+++ Rgb2Hires_PC/src/Common.h
@@ -23,18 +23,21 @@
 
 #include <Magick++.h>
 
+#define MAGICKCORE_ZERO_CONFIGURATION_SUPPORT 1
 
 namespace RgbToHires {
 
-	static Magick::Color WHITE("white");
-	static Magick::Color BLACK("black");
-	static Magick::Color BLUE("blue");
-	static Magick::Color GREEN("green");
-	static Magick::Color ORANGE("orange");
-	static Magick::Color VIOLET("purple");
 
-	static constexpr unsigned WIDTH = 140u;
-	static constexpr unsigned HEIGHT = 192u;
+	#define WHITE Magick::Color{"white"}
+	#define BLACK Magick::Color{"black"}
+	#define BLUE Magick::Color{"blue"}
+	#define GREEN Magick::Color{"green"}
+	#define ORANGE Magick::Color{"orange"}
+	#define VIOLET Magick::Color{"purple"}
+
+
+	constexpr unsigned WIDTH = 140u;
+	constexpr unsigned HEIGHT = 192u;
 
 }
 
